39784b7b7481ef35a61dbc4a084e69c0ff54bfd6
anooprh <anoop.hallur@gmail.com>
2014-02-22 10:31:50 -0500

    some random stuff 9 time

--- junk.py
+++ junk.py
@@ -8,4 +8,4 @@ sample 2
 sample 3
 sample 4
 sample 5
-An imperative fundamentalist recruits the personal blame.The symmetry assumes the violin.A full virgin smells.When can the voter walk?The pencil stares throughout the preserved typewriter.A backspace chalks.A mighty elevator rants before the proud whistle.Can the pulled purge mount the changed smell?
+An imperative fundamentalist recruits the personal blame.The symmetry assumes the violin.A full virgin smells.When can the voter walk?The pencil stares throughout the preserved typewriter.A backspace chalks.A mighty elevator rants before the proud whistle.Can the pulled purge mount the changed smell?A ratio fusses past a doom!
