50d2cf543be71a14227554def85734b779b2c1c8
anooprh <anoop.hallur@gmail.com>
2014-02-22 10:35:12 -0500

    misc

--- increase_push_frequency.sh
+++ increase_push_frequency.sh
@@ -1,20 +1,21 @@
   #!/bin/bash
-  for i in $(seq 1 $(shuf -i 10-30 -n 1))
+  for i in $(seq 1 $(shuf -i 1-4 -n 1))
   do
 	echo "\n"
     echo item: $i
-    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> abc
     curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.py
-    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.java
-    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.m
-    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.sh
-    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.hs
-    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.js
-    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.rb
-    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.pl
-    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.scala
-    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.jl
+#    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.java
+#    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.m
+#    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.sh
+#    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.hs
+#    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.js
+#    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.rb
+#    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.pl
+#    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.scala
+#    curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence >> junk.jl
+	
+	x= curl -H "Content-Length: 0" -X POST http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence
     git add .
-    git commit -m "some random stuff $i time"
+    git commit -m $x
     git push
   done
--- junk.java
+++ junk.java
@@ -8,4 +8,4 @@ sample 2
 sample 3
 sample 4
 sample 5
-The empire congests the drunk scroll in the universal.A tired saint tunnels the color.Will the significance riot the south analogy?Why does a stereotype bark?The museum trails within the archive!A fan works.Outside the broadcast tries the provoking leg.The misprint plants a threat behind the physic gene.The sandwich omits an exempt refrain.
+The empire congests the drunk scroll in the universal.A tired saint tunnels the color.Will the significance riot the south analogy?Why does a stereotype bark?The museum trails within the archive!A fan works.Outside the broadcast tries the provoking leg.The misprint plants a threat behind the physic gene.The sandwich omits an exempt refrain.Can the controller sole the lecture?
